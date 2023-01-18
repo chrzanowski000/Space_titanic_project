12b477ac343dc89ce02af38b97f44897f1cb9c70
Michal <chrzanowski000@gmail.com>
2023-01-18 22:12:10 +0100

    changed main

--- plan_main.py
+++ plan_main.py
@@ -1,6 +1,9 @@
-
+import load_data
+import Analyze
+import initialize_network
+import train
 def main():
-    train_data, test_data = Load_data(file_name)
-    Analyse(file_name)
-    Initialise_network()
-    Train(train_data, test_data, test=?, epoch_num=, )
+    train_data, test_data = load_data.load_data()
+    Analyze.analyze(file_name)
+    initialize_network.initialize_network()
+    train.train(train_data=train_data, test_data=test_data, test=False, epoch_num=10, )
