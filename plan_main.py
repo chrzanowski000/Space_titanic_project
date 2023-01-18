6a30481ea649d6d2038fa7f31a203243edaa0927
Michal <chrzanowski000@gmail.com>
2023-01-18 21:17:44 +0100

    .

--- plan_main.py
+++ plan_main.py
@@ -1,8 +1,6 @@
 
-
-
-main()
-  train_data, test_data = Load_data(file_name)
-  Analyse(file_name)
-  Initialise_network()
-  Train(train_data, test_data, test=?, epoch_num=, )
+def main():
+    train_data, test_data = Load_data(file_name)
+    Analyse(file_name)
+    Initialise_network()
+    Train(train_data, test_data, test=?, epoch_num=, )
