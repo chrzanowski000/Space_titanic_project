1c7c86749ade2b5a47d5b2b7be2dca93fcfa1f1a
Michal <chrzanowski000@gmail.com>
2023-03-01 20:09:40 +0100

    .

--- dataset.py
+++ dataset.py
@@ -1,5 +1,6 @@
 #Dataset class
 from torch.utils.data import Dataset # Dataset module
+import torch
 class Dataset_maker(Dataset):
     def __init__(self, data ,transform=False):
         self.data = data
@@ -9,8 +10,8 @@ class Dataset_maker(Dataset):
         return len(self.data)
 
     def __getitem__(self, index):
-        x = self.data[index][0]
-        y_label = self.data[index][1]
+        x = torch.tensor(self.data[index][0], requires_grad=True, dtype=torch.float64)
+        y_label = torch.tensor(self.data[index][1], requires_grad=True, dtype=torch.float64)
 
         if self.transform:
             x = self.transform(x)
